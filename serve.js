3ee8d8c2491bd17cd2187847d88af64583e1f7a6
danh <danh2331997@gmail.com>
2020-01-13 23:44:26 +0700

    upd src

--- serve.js
+++ serve.js
@@ -18,28 +18,29 @@ io.on('connection', (socket) => {
     lstbeforeUser.push(socket.room + "_" + socket.id)
     idCurRoom = 0
     count = 0
-    io.sockets.in(socket.id).emit('TypeChess', { text:"O", color: "red" })
-    io.sockets.in(socket.room).emit('StartGame', {indexUser: 2, startGame: true })
+    io.sockets.in(socket.id).emit('TypeChess', { text: "O", color: "red" })
+    io.sockets.in(socket.room).emit('StartGame', { indexUser: 2, startGame: true })
   } else {
     do {
       idCurRoom = Math.floor(Math.random() * 999999)
-    } while(idCurRoom === 3000 && lstUserInRoom.indexOf(idCurRoom) >= 0)
+    } while (idCurRoom === 3000 && lstUserInRoom.indexOf(idCurRoom) >= 0)
     socket.room = JSON.stringify(idCurRoom)
     socket.join(JSON.stringify(idCurRoom));
     lstUserInRoom.push(JSON.stringify(idCurRoom))
-    io.to(socket.id).emit('TypeChess', { text:"X", color: "black"})
+    io.to(socket.id).emit('TypeChess', { text: "X", color: "black" })
     io.sockets.in(socket.room).emit('StartGame', { indexUser: 1, startGame: false })
   }
+  socket.isGameOver = false
   socket.on('message', (pos, text) => {
     //check if is user before then not hadling
-    if (lstbeforeUser.length > 0 && lstbeforeUser.indexOf(socket.room + "_" + socket.id) >= 0 ){
+    if (lstbeforeUser.length > 0 && lstbeforeUser.indexOf(socket.room + "_" + socket.id) >= 0) {
       return
     }
 
     //upd user before by room 
     //room_idUser
-    for (let i = 0; i< lstbeforeUser.length ; i++) {
-      if (lstbeforeUser[i].split("_")[0] === socket.room){
+    for (let i = 0; i < lstbeforeUser.length; i++) {
+      if (lstbeforeUser[i].split("_")[0] === socket.room) {
         lstbeforeUser.splice(i, 1);//remove user before
         break;
       }
@@ -51,16 +52,22 @@ io.on('connection', (socket) => {
     beforeRoom = socket.room
   })
 
-  socket.on('disconnect', function() {
-    if (lstUserInRoom.indexOf(socket.room) >= 0){
+  socket.on('disconnect', function () {
+    if (lstUserInRoom.indexOf(socket.room) >= 0) {
       lstUserInRoom.splice(lstUserInRoom.indexOf(socket.room), 1)
     }
-    io.sockets.in(socket.room).emit('OutGame', { indexUser: 1, startGame: false })
+    if (socket.isGameOver !== true){
+      io.sockets.in(socket.room).emit('OutGame', { indexUser: 1, startGame: false })
+    }
     socket.leave(socket.room);
-    count-=1
+    count -= 1
     if (count < 0)
       count = 0
-});
+  });
+
+  socket.on('GameOver', function (isGameOver) {
+    socket.isGameOver = isGameOver
+  });
 
 })
 
