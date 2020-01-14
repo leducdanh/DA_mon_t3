3d106a675127c9acd2be8c1473e7ff1c9962abcb
danh <danh2331997@gmail.com>
2020-01-15 00:07:53 +0700

    upd src

--- serve.js
+++ serve.js
@@ -6,30 +6,63 @@ const io = require('socket.io')(http)
 app.use('/style', express.static(__dirname + '/style'))
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
-var lstUserInRoom = []
+var lstRoom = []
+var lstIdRoom = []
 var count = 0 //count user in room
 var idCurRoom = 0
 var lstbeforeUser = []
 io.on('connection', (socket) => {
-  count += 1
-  if (count >= 1 && idCurRoom !== 0) {
-    socket.room = JSON.stringify(idCurRoom)
-    socket.join(JSON.stringify(idCurRoom));
+  let index = -1
+  for (let i = 0; i < lstRoom.length; i++) {
+    if (lstRoom[i].UserInRoom.length < 2) {
+      index = i
+      break
+    }
+  }
+
+  if (index > -1) {
+    socket.room = lstRoom[index].idRoom
+    socket.join(lstRoom[index].idRoom);
+    lstRoom[index].UserInRoom.push(socket.id)
     lstbeforeUser.push(socket.room + "_" + socket.id)
     idCurRoom = 0
-    count = 0
     io.sockets.in(socket.id).emit('TypeChess', { text: "O", color: "red" })
     io.sockets.in(socket.room).emit('StartGame', { indexUser: 2, startGame: true })
   } else {
     do {
       idCurRoom = Math.floor(Math.random() * 999999)
-    } while (idCurRoom === 3000 && lstUserInRoom.indexOf(idCurRoom) >= 0)
+    } while (idCurRoom === 3000 && lstIdRoom.indexOf(idCurRoom) >= 0)
     socket.room = JSON.stringify(idCurRoom)
     socket.join(JSON.stringify(idCurRoom));
-    lstUserInRoom.push(JSON.stringify(idCurRoom))
+    let roomInfo = {
+      idRoom: JSON.stringify(idCurRoom),
+      UserInRoom: [socket.id]
+    }
+    lstRoom.push(roomInfo)
+    lstIdRoom.push(idCurRoom)
     io.to(socket.id).emit('TypeChess', { text: "X", color: "black" })
     io.sockets.in(socket.room).emit('StartGame', { indexUser: 1, startGame: false })
   }
+  //////////////////////////////
+  // count += 1
+  // if (count >= 1 && idCurRoom !== 0) {
+  //   socket.room = JSON.stringify(idCurRoom)
+  //   socket.join(JSON.stringify(idCurRoom));
+  //   lstbeforeUser.push(socket.room + "_" + socket.id)
+  //   idCurRoom = 0
+  //   count = 0
+  //   io.sockets.in(socket.id).emit('TypeChess', { text: "O", color: "red" })
+  //   io.sockets.in(socket.room).emit('StartGame', { indexUser: 2, startGame: true })
+  // } else {
+  //   do {
+  //     idCurRoom = Math.floor(Math.random() * 999999)
+  //   } while (idCurRoom === 3000 && lstRoom.indexOf(idCurRoom) >= 0)
+  //   socket.room = JSON.stringify(idCurRoom)
+  //   socket.join(JSON.stringify(idCurRoom));
+  //   lstRoom.push(JSON.stringify(idCurRoom))
+  //   io.to(socket.id).emit('TypeChess', { text: "X", color: "black" })
+  //   io.sockets.in(socket.room).emit('StartGame', { indexUser: 1, startGame: false })
+  // }
   socket.isGameOver = false
   socket.on('message', (pos, text) => {
     //check if is user before then not hadling
@@ -53,16 +86,27 @@ io.on('connection', (socket) => {
   })
 
   socket.on('disconnect', function () {
-    if (lstUserInRoom.indexOf(socket.room) >= 0) {
-      lstUserInRoom.splice(lstUserInRoom.indexOf(socket.room), 1)
-    }
-    if (socket.isGameOver !== true){
+    for(let i =0 ; i< lstRoom.length; i++) {
+      if (lstRoom[i].idRoom === socket.room){
+        lstRoom.splice(i, 1)
+        break
+      }
+    };
+    // if (lstRoom.indexOf(socket.room) >= 0) {
+    //   lstRoom.splice(lstRoom.indexOf(socket.room), 1)
+    // }
+    if (socket.isGameOver !== true) {
       io.sockets.in(socket.room).emit('OutGame', { indexUser: 1, startGame: false })
     }
     socket.leave(socket.room);
     count -= 1
     if (count < 0)
       count = 0
+  });
+
+  
+  socket.on('timeStop', function (text) {
+    // console.log(text)
   });
 
   socket.on('GameOver', function (isGameOver) {
